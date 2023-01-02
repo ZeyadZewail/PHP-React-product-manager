3b91182d615a8acbbdca4a57b761c9e2d198a343
Ktz <ZeyadZewail@gmail.com>
2023-01-02 22:58:12 +0200

    Made Cards responsive in a grid

--- React(TS) frontend/src/components/Card/Card.tsx
+++ React(TS) frontend/src/components/Card/Card.tsx
@@ -1,5 +1,6 @@
-import React, {FunctionComponent,useState,useEffect} from 'react'
-import Product from '../../Interfaces/Product.interface'
+import React, {FunctionComponent,useState,useEffect} from 'react';
+import Product from '../../Interfaces/Product.interface';
+import './Card.css';
 
 interface CardInterface{
     Product: Product
@@ -8,7 +9,12 @@ interface CardInterface{
 const Card: FunctionComponent<CardInterface> = ({Product}) => {
     
   return (
-    <div>{Product.Name}</div>
+    <div className='card'>
+      <p>{Product.SKU}</p>
+      <p>{Product.Name}</p>
+      <p>{Product.Price}$</p>
+      <p>{Product.TypeValue}</p>
+    </div>
   )
 }
 
--- React(TS) frontend/src/components/CardHolder/CardHolder.tsx
+++ React(TS) frontend/src/components/CardHolder/CardHolder.tsx
@@ -1,6 +1,7 @@
 import React, {FunctionComponent,useState,useEffect} from 'react'
 import Product from '../../Interfaces/Product.interface'
 import Card from '../Card/Card'
+import './CardHolder.css'
 
 interface CardsHolderInterface{
     Products: Product[]
@@ -8,12 +9,20 @@ interface CardsHolderInterface{
 
 const CardHolder: FunctionComponent<CardsHolderInterface> = ({Products}) => {
 
+    const loadingCheck = () =>{
+      if(Products.length==0){
+        return <div className='loading'><h1>Loading....</h1></div>
+      }else{
+        return <div className='cardholder'>{cards}</div>
+      }
+    }
+
     const cards = Products.map((item)=> <Card key={item.Name} Product={item}/>);
 
   return (
-    <div>
-        {cards}
-    </div>
+    <React.Fragment>
+      {loadingCheck()}
+    </React.Fragment>
   )
 }
 
